560d2efd1f680057aeaf80e9a39569af78831353
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-09 21:52:07 +0200

    Setting up back-end application

---
+++ Backend/Little ToDo List/Little ToDo List.Application/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Little_ToDo_List.Application;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
