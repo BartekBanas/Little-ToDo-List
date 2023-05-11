c8b7934b8b4338ae7d9f542481da8fac906dac1d
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-11 23:11:13 +0200

    Basic Get endpoints

---
+++ Backend/Little ToDo List/LittleToDoList.Api/Controllers/ToDoTaskController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LittleToDoList.Api.Controllers;
+
+[ApiController]
+[Route("api/task")]
+public class ToDoTaskController
+{
+    [HttpGet]
+    public async Task<IActionResult> ReturnAllTasks()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{taskId:int}")]
+    public async Task<IActionResult> ReturnSpecificTask([FromRoute] int taskId)
+    {
+        throw new NotImplementedException();
+    }
+}
