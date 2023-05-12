f037b72bd0395312e1a95f0dbb6003c4fd74191b
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-12 22:21:04 +0200

    Adding DTO

---
+++ Backend/Little ToDo List/LittleToDoList.Api/LittleToDoList.Application.Dto/ToDoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LittleToDoList.Application.Dto;
+
+public class ToDoDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime CompletionDate { get; set; }
+    public string Description { get; set; }
+    public bool IsDone { get; set; }
+}
