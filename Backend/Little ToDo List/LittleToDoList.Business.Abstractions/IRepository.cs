8d9ef841d01df5f774f5bd97cdd9878e7f63278b
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-15 20:51:30 +0200

    Implement inherited methods in interface

--- Backend/Little ToDo List/LittleToDoList.Business.Abstractions/IRepository.cs
+++ Backend/Little ToDo List/LittleToDoList.Business.Abstractions/IRepository.cs
@@ -5,4 +5,11 @@ public interface IRepository { }
 public interface IRepository<TEntity> : IRepository where TEntity : IEntity
 {
     Task<TEntity?> GetOneAsync(int id);
+
+    public Task<ICollection<TEntity>> GetAllAsync();
+
+
+    public Task DeleteOneAsync(int keys);
+
+    public Task<TEntity> CreateOneAsync(TEntity entity);
 }
