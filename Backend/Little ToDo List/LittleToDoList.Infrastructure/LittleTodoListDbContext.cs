4beda113d582081fe81b01a4a63a038f3d2e824d
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-16 18:42:24 +0200

    LittleTodoListDbContext created

--- Backend/Little ToDo List/LittleToDoList.Infrastructure/LittleTodoListDbContext.cs
+++ Backend/Little ToDo List/LittleToDoList.Infrastructure/LittleTodoListDbContext.cs
@@ -1,5 +1,32 @@
-﻿namespace LittleToDoList.Infrastructure;
+﻿using LittleToDoList.Business.Entities;
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
 
-public class LittleTodoListDbContext
+namespace LittleToDoList.Infrastructure;
+
+public class LittleTodoListDbContext : DbContext
 {
+    public DbSet<TaskItem> Todos { get; set; } = null!;
+    
+    public LittleTodoListDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        try
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+        catch (DbUpdateException dbUpdateException)
+        {
+            var sqlException = dbUpdateException.InnerException as MySqlException ?? throw dbUpdateException;
+            
+            // Violation of DISTINCT constraint 
+            if (sqlException.Number == 1062)
+                throw new Exception("Item duplicated");
+
+            throw;
+        }
+    }
 }
