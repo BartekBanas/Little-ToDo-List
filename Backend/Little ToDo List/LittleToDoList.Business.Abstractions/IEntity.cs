09c5591375ebaf490950d1781a754489eb8180ae
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-16 18:13:54 +0200

    Expanding Entity

--- Backend/Little ToDo List/LittleToDoList.Business.Abstractions/IEntity.cs
+++ Backend/Little ToDo List/LittleToDoList.Business.Abstractions/IEntity.cs
@@ -2,5 +2,7 @@
 
 public interface IEntity
 {
-    
+    public IReadOnlyCollection<IDomainEvent>? DomainEvents { get; }
+    public void AddDomainEvent(IDomainEvent notification);
+    public void ClearDomainEvents();
 }
