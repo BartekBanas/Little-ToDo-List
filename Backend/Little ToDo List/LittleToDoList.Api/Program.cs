6a7e5b6763238d3d9dbd6b54bccf03f7fe0623d9
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-19 22:50:38 +0200

    Program.cs cleanup

--- Backend/Little ToDo List/LittleToDoList.Api/Program.cs
+++ Backend/Little ToDo List/LittleToDoList.Api/Program.cs
@@ -5,9 +5,7 @@ using LittleToDoList.Api.Controllers;
 using LittleToDoList.Application.Services;
 using LittleToDoList.Business.Abstractions;
 using LittleToDoList.Business.Entities;
-using LittleToDoList.Business.DomainEventHandlers;
 using LittleToDoList.Infrastructure.Repositories;
-using AutoMapper;
 using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -39,7 +37,6 @@ services.AddMediatR(cfg =>
 {
     cfg.RegisterServicesFromAssemblyContaining<LittleToDoList.Business.DomainEventHandlers.AssemblyMarker>();
 });
-services.AddScoped<IMediator, Mediator>();
 
 // ============= RUN =============
 var app = builder.Build();
