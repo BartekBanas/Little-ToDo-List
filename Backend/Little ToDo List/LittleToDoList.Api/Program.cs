9959674373581c51746bb3aa4baeb21aa788b9b5
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-19 22:05:11 +0200

    Added scopes for services in the app

--- Backend/Little ToDo List/LittleToDoList.Api/Program.cs
+++ Backend/Little ToDo List/LittleToDoList.Api/Program.cs
@@ -1,6 +1,14 @@
-using LittleToDoList.Infrastructure;
+using LittleToDoList.Api;
 using Microsoft.EntityFrameworkCore;
+using LittleToDoList.Infrastructure;
 using LittleToDoList.Api.Controllers;
+using LittleToDoList.Application.Services;
+using LittleToDoList.Business.Abstractions;
+using LittleToDoList.Business.Entities;
+using LittleToDoList.Business.DomainEventHandlers;
+using LittleToDoList.Infrastructure.Repositories;
+using AutoMapper;
+using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +28,18 @@ services.AddDbContext<LittleTodoListDbContext>(contextOptionsBuilder =>
         configuration.GetConnectionString("LittleTodoListDatabaseConnectionString"),
         new MySqlServerVersion(new Version(8, 0, 28))
     ));
+
+services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+
+services.AddScoped<IRepository<TaskItem>,     Repository<TaskItem,  LittleTodoListDbContext>>();
+
+services.AddScoped<IToDoItemService, ToDoItemService>();
+
+services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssemblyContaining<LittleToDoList.Business.DomainEventHandlers.AssemblyMarker>();
+});
+services.AddScoped<IMediator, Mediator>();
 
 // ============= RUN =============
 var app = builder.Build();
