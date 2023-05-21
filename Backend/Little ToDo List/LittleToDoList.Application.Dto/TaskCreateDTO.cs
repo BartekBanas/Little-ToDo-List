c7fa806e5135b75f3ada98ad13c9d70dc5f6f454
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-21 21:36:26 +0200

    Add new DTO for creating todo task

---
+++ Backend/Little ToDo List/LittleToDoList.Application.Dto/TaskCreateDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace LittleToDoList.Application.Dto;
+
+public class TaskCreateDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+
+    public TaskCreateDto(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public TaskCreateDto(int id, string name, string? description)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+    }
+}
