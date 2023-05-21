f7041df83cccd3c4f341ccaba2ec64da27c07f12
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-21 21:44:07 +0200

    TaskCreateDto fix

--- Backend/Little ToDo List/LittleToDoList.Application.Dto/TaskCreateDTO.cs
+++ Backend/Little ToDo List/LittleToDoList.Application.Dto/TaskCreateDTO.cs
@@ -2,19 +2,16 @@
 
 public class TaskCreateDto
 {
-    public int Id { get; set; }
     public string Name { get; set; }
     public string? Description { get; set; }
 
-    public TaskCreateDto(int id, string name)
+    public TaskCreateDto(string name)
     {
-        Id = id;
         Name = name;
     }
 
-    public TaskCreateDto(int id, string name, string? description)
+    public TaskCreateDto(string name, string? description)
     {
-        Id = id;
         Name = name;
         Description = description;
     }
