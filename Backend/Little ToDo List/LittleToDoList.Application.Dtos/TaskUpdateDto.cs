cd7186183b03c9aba305157ebff0c0fba9d49410
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-24 23:33:38 +0200

    Add TaskUpdateDto

--- Backend/Little ToDo List/LittleToDoList.Application.Dtos/TaskUpdateDto.cs
+++ Backend/Little ToDo List/LittleToDoList.Application.Dtos/TaskUpdateDto.cs
@@ -2,5 +2,16 @@
 
 public class TaskUpdateDto
 {
+    public string? Name { get; set; }
+    public DateTime? CompletionDate { get; set; }
+    public string? Description { get; set; }
+    public bool? IsDone { get; set; }
     
+    public TaskUpdateDto(string? name, DateTime? completionDate, string? description, bool? isDone)
+    {
+        Name = name;
+        CompletionDate = completionDate;
+        Description = description;
+        IsDone = isDone;
+    }
 }
