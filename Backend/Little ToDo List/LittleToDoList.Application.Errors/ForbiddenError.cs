ec10f67eccd959293f3252fa8a7106f6b0e6e20f
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-24 22:42:44 +0200

    Adding Custom error exceptions

---
+++ Backend/Little ToDo List/LittleToDoList.Application.Errors/ForbiddenError.cs
@@ -0,0 +1,14 @@
+﻿using LittleToDoList.Application.Errors.Abstractions;
+
+namespace LittleToDoList.Application.Errors;
+
+public class ForbiddenError : ErrorException
+{
+    public ForbiddenError()
+    {
+    }
+
+    public ForbiddenError(string? message) : base(message)
+    {
+    }
+}
