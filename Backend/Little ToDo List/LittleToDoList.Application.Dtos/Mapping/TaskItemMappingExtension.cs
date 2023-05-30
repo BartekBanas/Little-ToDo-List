9cf5456623d31c84028d9d9ddf7cb607e3c37b9b
Bartek Banaś <bartek.banas675@gmail.com>
2023-05-30 16:38:49 +0200

    Add mapping extension

---
+++ Backend/Little ToDo List/LittleToDoList.Application.Dtos/Mapping/TaskItemMappingExtension.cs
@@ -0,0 +1,17 @@
+﻿using LittleToDoList.Business.Entities;
+
+namespace LittleToDoList.Application.Dto.Mapping;
+
+public static class TaskItemMappingExtension
+{
+    public static TaskItemDto ToDto(this TaskItem entity)
+    {
+        return new TaskItemDto(entity.Id, entity.Name, entity.CreationDate)
+        {
+            CompletionDate = entity.CompletionDate,
+            Description = entity.Description,
+            IsDone = entity.IsDone
+        };
+    }
+    
+}
