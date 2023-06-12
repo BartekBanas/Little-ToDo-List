ca6f3c01bcff42fdb196e90d41b9e90498505e7b
Bartek Banaś <bartek.banas675@gmail.com>
2023-06-12 17:05:34 +0200

    Add User entity

---
+++ Backend/Little ToDo List/LittleToDoList.Business.Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using LittleToDoList.Business.Abstractions;
+
+namespace LittleToDoList.Business.Entities;
+
+public class User : Entity
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public DateTime CreationDate { get; set; }
+}
