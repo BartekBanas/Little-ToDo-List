c25587f661a34b38e63874123a67df37907faf94
Bartek Banaś <bartek.banas675@gmail.com>
2023-06-12 18:35:47 +0200

    Add User Dtos

---
+++ Backend/Little ToDo List/LittleToDoList.Application.Dtos/UserCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LittleToDoList.Application.Dto;
+
+public class UserCreateDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+}
---
+++ Backend/Little ToDo List/LittleToDoList.Application.Dtos/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LittleToDoList.Application.Dto;
+
+public class UserDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public DateTime CreationDate { get; set; }
+}
