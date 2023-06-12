8145c0770eb1b8a756cc1a26851e648eb47770ca
Bartek Banaś <bartek.banas675@gmail.com>
2023-06-12 19:13:39 +0200

    Add mapping to UserDTO

---
+++ Backend/Little ToDo List/LittleToDoList.Application.Dtos/Mapping/UserMappingExtension.cs
@@ -0,0 +1,16 @@
+﻿using LittleToDoList.Business.Entities;
+
+namespace LittleToDoList.Application.Dto.Mapping;
+
+public static class UserMappingExtension
+{
+    public static UserDto ToDto(this User entity)
+    {
+        return new UserDto(entity.Id, entity.Name, entity.CreationDate)
+        {
+            CreationDate = entity.CreationDate,
+            Name = entity.Name,
+            Id = entity.Id
+        };
+    }
+}
--- Backend/Little ToDo List/LittleToDoList.Application.Dtos/UserDto.cs
+++ Backend/Little ToDo List/LittleToDoList.Application.Dtos/UserDto.cs
@@ -4,6 +4,17 @@ public class UserDto
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = null!;
-    public string PasswordHash { get; set; } = null!;
     public DateTime CreationDate { get; set; }
+
+    public UserDto()
+    {
+        
+    }
+
+    public UserDto(Guid id, string name, DateTime creationDate)
+    {
+        Id = id;
+        Name = name;
+        CreationDate = creationDate;
+    }
 }
