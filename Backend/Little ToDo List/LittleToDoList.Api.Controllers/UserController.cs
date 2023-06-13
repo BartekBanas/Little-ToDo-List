1293fbee952c1d736e5f246cb0d9a77056c13eba
Bartek Banaś <bartek.banas675@gmail.com>
2023-06-13 19:49:23 +0200

    Add CRUD for user

--- Backend/Little ToDo List/LittleToDoList.Api.Controllers/UserController.cs
+++ Backend/Little ToDo List/LittleToDoList.Api.Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using LittleToDoList.Application.Services;
+﻿using LittleToDoList.Application.Dto;
+using LittleToDoList.Application.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LittleToDoList.Api.Controllers;
@@ -12,5 +13,45 @@ public class UserController : Controller
     public UserController(IUserService userService)
     {
         _userService = userService;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var users = await _userService.GetAllUsersAsync();
+
+        return Ok(users);
+    }
+    
+    [HttpGet("{userId:Guid}")]
+    public async Task<IActionResult> ReturnSpecificUser([FromRoute] Guid userId)
+    {
+        var user = await _userService.GetUserAsync(userId);
+
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateUser([FromBody] UserCreateDto userDto)
+    {
+        await _userService.CreateUser(userDto);
+
+        return Ok();
+    }
+    
+    [HttpPut("{userId:Guid}")]
+    public async Task<IActionResult> UpdateUser([FromRoute] Guid userId, [FromBody] UserUpdateDto updateDto)
+    {
+        var updatedUser = await _userService.UpdateUserAsync(userId, updateDto);
+
+        return Ok(updatedUser);
+    }
+    
+    [HttpDelete("{userId:Guid}")]
+    public async Task<IActionResult> DeleteUser([FromRoute] Guid userId)
+    {
+        await _userService.DeleteUser(userId);
+
+        return Ok();
     }
 }
