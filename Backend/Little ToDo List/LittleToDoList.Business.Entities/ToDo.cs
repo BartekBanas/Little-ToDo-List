4d3388007bae5f002e80ca36819b75b8129d4221
Bartek Banaś <bartek.banas675@gmail.com>
2023-06-13 21:27:43 +0200

    Add annotations

--- Backend/Little ToDo List/LittleToDoList.Business.Entities/ToDo.cs
+++ Backend/Little ToDo List/LittleToDoList.Business.Entities/ToDo.cs
@@ -1,15 +1,20 @@
-﻿using LittleToDoList.Business.Abstractions;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using LittleToDoList.Business.Abstractions;
 
 namespace LittleToDoList.Business.Entities;
 
 public class ToDo : Entity
 {
+    [Key]
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public DateTime CreationDate { get; set; }
     public DateTime? CompletionDate { get; set; }
     public string? Description { get; set; }
     public bool IsDone { get; set; }
+    
+    [ForeignKey(nameof(User))]
     public Guid AssignedUserId  { get; set; }
 
     private ToDo()
