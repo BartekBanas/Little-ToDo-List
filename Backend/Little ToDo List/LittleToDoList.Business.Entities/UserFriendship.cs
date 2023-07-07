4e35b45cb2433c61f48bec332a98f0040f1a2295
Bartek Banaś <bartek.banas675@gmail.com>
2023-07-07 23:48:35 +0200

    Add UserFriendship

---
+++ Backend/Little ToDo List/LittleToDoList.Business.Entities/UserFriendship.cs
@@ -0,0 +1,16 @@
+﻿using LittleToDoList.Business.Abstractions;
+
+namespace LittleToDoList.Business.Entities;
+
+public class UserFriendship : Entity
+{
+    public Guid Id { get; set; }
+    
+    public Guid FirstUserId { get; set; }
+    public virtual User FirstUser { get; set; } = null!;
+
+    public Guid SecondUserId { get; set; }
+    public virtual User SecondUser { get; set; } = null!;
+
+    public DateTime DateCreated { get; set; }
+}
