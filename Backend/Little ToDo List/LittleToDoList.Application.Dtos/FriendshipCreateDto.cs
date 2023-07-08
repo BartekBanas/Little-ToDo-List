c4feb121f6d6435f2b5678b216ce2f8f7c5f63f9
Bartek Banaś <bartek.banas675@gmail.com>
2023-07-08 03:52:07 +0200

    Add Friendship Create Dto

---
+++ Backend/Little ToDo List/LittleToDoList.Application.Dtos/FriendshipCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LittleToDoList.Application.Dto;
+
+public class FriendshipCreateDto
+{
+    public Guid Id { get; set; }
+    
+    public Guid FirstUserId { get; set; }
+
+    public Guid SecondUserId { get; set; }
+}
