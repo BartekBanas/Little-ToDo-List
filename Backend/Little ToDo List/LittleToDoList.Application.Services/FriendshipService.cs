74163b806979066bf1b7dd725a6a56126ead3350
Bartek Banaś <bartek.banas675@gmail.com>
2023-07-08 14:20:57 +0200

    Add basic Friendship Service

---
+++ Backend/Little ToDo List/LittleToDoList.Application.Services/FriendshipService.cs
@@ -0,0 +1,31 @@
+﻿using LittleToDoList.Business.Abstractions;
+using LittleToDoList.Business.Entities;
+
+namespace LittleToDoList.Application.Services;
+
+public interface IFriendshipService
+{
+    Task CreateFriendship(Guid firstAccountId, Guid secondAccountId);
+}
+
+public class FriendshipService : IFriendshipService
+{
+    private readonly IRepository<UserFriendship> _friendshipRepository;
+
+    public FriendshipService(IRepository<UserFriendship> friendshipRepository)
+    {
+        _friendshipRepository = friendshipRepository;
+    }
+
+    public async Task CreateFriendship(Guid firstAccountId, Guid secondAccountId)
+    {
+        var newFriendship = UserFriendship.CreateInstance(
+            firstUserId: firstAccountId,
+            secondUserId: secondAccountId
+        );
+
+        await _friendshipRepository.CreateOneAsync(newFriendship);
+
+        await _friendshipRepository.SaveChangesAsync();
+    }
+}
